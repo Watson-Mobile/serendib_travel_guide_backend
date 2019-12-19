9dbce24c3f818b44a3892f9016a64dd8f9e644eb
Piyumi Sudusinghe <piyumisudusinghe@gmail.com>
2019-12-19 14:40:22 +0530

    updated comments

--- controllers/user.controller.js
+++ controllers/user.controller.js
@@ -1,6 +1,6 @@
 const User = require('../models/user.model');
 
-//Simple version, without validation or sanitation
+
 exports.user = function (req, res) {
     res.send('Greetings from the User controller!');
 };
