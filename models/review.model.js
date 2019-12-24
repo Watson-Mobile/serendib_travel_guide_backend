25f7a02111985976e001cfec50351cc6dcaa4a6a
supimi <supimi.15@cse.mrt.ac.lk>
2019-12-24 11:24:14 +0530

    review mpdel updated.

--- models/review.model.js
+++ models/review.model.js
@@ -6,6 +6,7 @@ const ReviewSchema = new Schema({
     comment: {type:String, max:200},
     rating: {type:Number, required:true},
     image:{type:[String]},
+    place:{type:Schema.Types.ObjectId,ref:'Place', required:true},
     user_posted:{type:Schema.Types.ObjectId,ref:'User', required:true},
     date_posted:{type: Date, required:true, default: Date.now}
 
